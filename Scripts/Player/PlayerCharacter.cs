102bc6cd2040e3a60f59f3e9e1f406429544285f
Roger <roger@dalkefamily.com>
2023-11-08 02:27:25 -0800

    Changed line spacing?
    Not sure why this happened but it won't stay discarded.

--- Scripts/Player/PlayerCharacter.cs
+++ Scripts/Player/PlayerCharacter.cs
@@ -5,15 +5,15 @@ public partial class PlayerCharacter : CharacterBody2D, IHealth
 	[ExportCategory("Stats")]
 	[Export]
 	private float speed = 400f;
-    [Export]
-    public int MaxHealth { get; private set; }
-    public float CurrHealth { get; private set; }
+	[Export]
+	public int MaxHealth { get; private set; }
+	public float CurrHealth { get; private set; }
 
 	[ExportCategory("Refs")]
-    [Export]
-    private AnimationPlayer anim;
+	[Export]
+	private AnimationPlayer anim;
 
-    public static PlayerCharacter Instance { get; private set; }
+	public static PlayerCharacter Instance { get; private set; }
 
 	public override void _Ready()
 	{
@@ -28,53 +28,58 @@ public partial class PlayerCharacter : CharacterBody2D, IHealth
 		}
 
 		//IHealth
-        if (MaxHealth <= 0)
-        {
-            GD.PrintErr("Player health must be greater than 0.");
-        }
-        CurrHealth = MaxHealth;
+		if (MaxHealth <= 0)
+		{
+			GD.PrintErr("Player health must be greater than 0.");
+		}
+		CurrHealth = MaxHealth;
 
 		//Animation
-        if (!IsInstanceValid(anim))
-        {
-            GD.PrintErr("No animation player");
-        }
-    }
+		if (!IsInstanceValid(anim))
+		{
+			GD.PrintErr("No animation player");
+		}
+	}
 
-    public void TakeDamage(float amount)
-    {
-        if (amount <= 0)
-        {
-            GD.PrintErr("Damage must be for a positive amount.");
-            return;
-        }
+	public void TakeDamage(float amount)
+	{
+		if (amount <= 0)
+		{
+			GD.PrintErr("Damage must be for a positive amount.");
+			return;
+		}
 
-        CurrHealth -= amount;
-        GD.Print("Player took damage.");
-        if (CurrHealth <= 0)
-        {
-            Die();
-        }
-    }
+		CurrHealth -= amount;
+		GD.Print("Player took damage.");
+		if (CurrHealth <= 0)
+		{
+			Die();
+		}
+	}
 
-    public void Heal(float amount)
-    {
-        if (amount <= 0)
-        {
-            GD.PrintErr("Healing must be for a positive amount.");
-            return;
-        }
+	public void Heal(float amount)
+	{
+		if (amount <= 0)
+		{
+			GD.PrintErr("Healing must be for a positive amount.");
+			return;
+		}
 
-        CurrHealth = Mathf.Clamp(CurrHealth + amount, 0f, MaxHealth);
-    }
+		CurrHealth = Mathf.Clamp(CurrHealth + amount, 0f, MaxHealth);
+	}
 
-    private void Die()
-    {
-        GD.Print("Player dead");
-        //Game Over.exe
-    }
+	public float GetHealthPercent()
+	{
+		return Mathf.Clamp(CurrHealth, 0, MaxHealth) / MaxHealth;
+	}
 
-    public override void _PhysicsProcess(double delta)
+	private void Die()
+	{
+		GD.Print("Player dead");
+		//Game Over.exe
+	}
+
+	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 newVelocity = Vector2.Zero;
 
@@ -84,29 +89,29 @@ public partial class PlayerCharacter : CharacterBody2D, IHealth
 		{
 			//If moving, set velocity
 			newVelocity = direction * speed;
-            //GD.Print(direction + " * " + speed + " = " + newVelocity);
+			//GD.Print(direction + " * " + speed + " = " + newVelocity);
 		}
 
 		this.Velocity = newVelocity;
 		MoveAndSlide();
-    }
+	}
 
-    public override void _Process(double delta)
-    {
-        if (Input.IsActionJustPressed("Attack"))
-        {
-            Attack();
-        }
-    }
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("Attack"))
+		{
+			Attack();
+		}
+	}
 
-    private void Attack()
-    {
-        GD.Print("Player attacked");
-        anim.Play(name: "basic attack");
-    }
+	private void Attack()
+	{
+		GD.Print("Player attacked");
+		anim.Play(name: "basic attack");
+	}
 
-    public override void _ExitTree()
-    {
-        if (Instance == this) { Instance = null; }
-    }
+	public override void _ExitTree()
+	{
+		if (Instance == this) { Instance = null; }
+	}
 }
